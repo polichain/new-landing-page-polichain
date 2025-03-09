2c8156f08fe7016793af95ae943fa69130a0cd64
marinhotg <tiago.mlima@hotmail.com>
2025-03-09 00:58:07 -0300

    refactor members page and add support positions title translations

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,5 @@
 import { ui, defaultLang, showDefaultLang } from './ui';
+import type { TranslationKey } from './ui';
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
@@ -7,7 +8,7 @@ export function getLangFromUrl(url: URL) {
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
+  return function t(key: TranslationKey) {
     return ui[lang][key] || ui[defaultLang][key];
   }
 }
@@ -15,6 +16,6 @@ export function useTranslations(lang: keyof typeof ui) {
 export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
     if (path.startsWith('/')) path = path.slice(1);
-    return l === defaultLang ? `/${path}` : `/${l}/${path}`;
+    return `/${l}/${path}`;
   }
 }
